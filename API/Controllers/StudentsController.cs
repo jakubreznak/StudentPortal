f84ab7b15b15f73c60cfbeb98949abe6cc17a403
jakubreznak <james1998@seznam.cz>
2020-12-15 13:18:35 +0100

    prvni commit (API)

---
+++ API/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public StudentsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            return await _context.Students.FindAsync(id);
+        }
+    }
+}
---
+++ API/Entities/Student.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string upolNumber { get; set; }
+    }
+}

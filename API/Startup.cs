2246036998ae1008c40a7c15ee95911ae9c85208
jakubreznak <james1998@seznam.cz>
2021-04-14 22:03:59 +0200

    CSP remove

--- API/Startup.cs
+++ API/Startup.cs
@@ -82,7 +82,7 @@ namespace API
                 context.Response.Headers.Add("X-Frame-Options", "DENY");
                 context.Response.Headers.Add("X-Xss-Protection", "1; mode=block");
                 context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
+                //context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
 
                 await next();
             });

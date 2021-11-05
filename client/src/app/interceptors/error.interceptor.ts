221fdb9a3ac92bd5c7439da86e55428c8458e1cd
jakubreznak <james1998@seznam.cz>
2021-11-05 20:49:50 +0100

    Error CORS toastr

--- client/src/app/interceptors/error.interceptor.ts
+++ client/src/app/interceptors/error.interceptor.ts
@@ -15,13 +15,28 @@ export class ErrorInterceptor implements HttpInterceptor {
   constructor(private toastr: ToastrService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(
-      catchError(error => {
-        if (error) {
-          this.toastr.error(error.error);
-        }
-        return throwError(error);
-      })
-    );
+    
+    if(request.url.includes("stagservices.upol.cz"))
+    {
+      return next.handle(request).pipe(
+        catchError(error => {
+          if (error) {
+            this.toastr.error("Zkontrolujte že se nacházíte ve vnitřní síti a máte vypnuté CORS.");
+          }
+          return throwError(error);
+        })
+      );
+    }
+    else
+    {
+      return next.handle(request).pipe(
+        catchError(error => {
+          if (error) {
+            this.toastr.error(error.error);
+          }
+          return throwError(error);
+        })
+      );
+    }
   }
 }
